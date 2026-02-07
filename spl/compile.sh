1a713c31405ae238ade6a1032c33b860f48d803e
mihirpa18 <punchithayamedhamihir@gmail.com>
2026-02-07 13:57:41 +0000

    Stage-20 fork done

---
+++ spl/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+./spl spl_progs/boot_module-7.spl
+./spl spl_progs/osStartup.spl
+./spl spl_progs/console_interrupt.spl
+./spl spl_progs/deviceManager_module-4.spl
+./spl spl_progs/disk_interrupt.spl
+./spl spl_progs/exception_handler.spl
+./spl spl_progs/int6_read.spl
+./spl spl_progs/int7_write.spl
+./spl spl_progs/int8_fork.spl
+./spl spl_progs/int9_exec.spl
+./spl spl_progs/int10_exit.spl
+./spl spl_progs/memoryManager_module-2.spl
+./spl spl_progs/processManager_module-1.spl
+./spl spl_progs/resourceManager_module-0.spl
+./spl spl_progs/scheduler_module-5.spl
+./spl spl_progs/timer_interrupt.spl
+
+echo "All SPL files compiled successfully."
