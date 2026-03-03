eebe556fe579751a11a3527ab06fa9441534c147
mihirpa18 <punchithayamedhamihir@gmail.com>
2026-03-03 11:38:31 +0000

    Stage-22 done

--- spl/compile.sh
+++ spl/compile.sh
@@ -14,6 +14,8 @@ set -e
 ./spl spl_progs/int9_exec.spl
 ./spl spl_progs/int10_exit.spl
 ./spl spl_progs/int11_access.spl
+./spl spl_progs/int13_semGetRel.spl
+./spl spl_progs/int14_semlockUnlock.spl
 ./spl spl_progs/int15_shutdown.spl
 ./spl spl_progs/memoryManager_module-2.spl
 ./spl spl_progs/processManager_module-1.spl
