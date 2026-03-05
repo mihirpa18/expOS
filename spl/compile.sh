cfcd99da186dd34579a4ed0749f94da26ed519b2
mihirpa18 <punchithayamedhamihir@gmail.com>
2026-03-05 13:46:55 +0000

    Stage-23 done

--- spl/compile.sh
+++ spl/compile.sh
@@ -2,25 +2,26 @@
 
 set -e
 
-./spl spl_progs/assignments/stage-22/assg3/boot_module-7.spl
+./spl spl_progs/boot_module-7.spl
 ./spl spl_progs/osStartup.spl
-./spl spl_progs/assignments/stage-22/assg3/console_interrupt.spl
+./spl spl_progs/console_interrupt.spl
 ./spl spl_progs/deviceManager_module-4.spl
-./spl spl_progs/assignments/stage-22/assg3/disk_interrupt.spl
-./spl spl_progs/assignments/stage-22/assg3/exception_handler.spl
-./spl spl_progs/assignments/stage-22/assg3/int6_read.spl
-./spl spl_progs/assignments/stage-22/assg3/int7_write.spl
-./spl spl_progs/assignments/stage-22/assg3/int8_fork.spl
-./spl spl_progs/assignments/stage-22/assg3/int9_exec.spl
-./spl spl_progs/assignments/stage-22/assg3/int10_exit.spl
-./spl spl_progs/assignments/stage-22/assg3/int11_access.spl
-./spl spl_progs/assignments/stage-22/assg3/int13_semGetRel.spl
-./spl spl_progs/assignments/stage-22/assg3/int14_semlockUnlock.spl
-./spl spl_progs/assignments/stage-22/assg3/int15_shutdown.spl
+./spl spl_progs/disk_interrupt.spl
+./spl spl_progs/exception_handler.spl
+./spl spl_progs/int4_createDelete.spl
+./spl spl_progs/int6_read.spl
+./spl spl_progs/int7_write.spl
+./spl spl_progs/int8_fork.spl
+./spl spl_progs/int9_exec.spl
+./spl spl_progs/int10_exit.spl
+./spl spl_progs/int11_access.spl
+./spl spl_progs/int13_semGetRel.spl
+./spl spl_progs/int14_semlockUnlock.spl
+./spl spl_progs/int15_shutdown.spl
 ./spl spl_progs/memoryManager_module-2.spl
 ./spl spl_progs/processManager_module-1.spl
-./spl spl_progs/assignments/stage-22/assg3/resourceManager_module-0.spl
-./spl spl_progs/assignments/stage-22/assg3/scheduler_module-5.spl
-./spl spl_progs/assignments/stage-22/assg3/timer_interrupt.spl
+./spl spl_progs/resourceManager_module-0.spl
+./spl spl_progs/scheduler_module-5.spl
+./spl spl_progs/timer_interrupt.spl
 
 echo "All SPL files compiled successfully."
